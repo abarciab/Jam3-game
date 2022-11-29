fb76ff05bbc41fd24345ff3b8f05b1fb647c0a5e
Koobysnack <97823937+Koobysnack@users.noreply.github.com>
2022-11-29 15:35:11 -0800

    Camera Panning Prefab

--- Assets/scripts/FreeRoam/CameraPan.cs
+++ Assets/scripts/FreeRoam/CameraPan.cs
@@ -6,18 +6,21 @@ public class CameraPan : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Camera mainCam;
+    [SerializeField]private Transform panCam;
     [SerializeField] private float speed;
+    [Tooltip("Note: Z must a large enough negative number to view entire tilemap")]
     [SerializeField] private List<Vector3> panPositions;
 
-    [SerializeField]private Transform panCam;
     private BoxCollider2D box;
     private Vector3 mainCamPos;
     private bool activated;
+    private bool goingBack;
     private int listIndex;
 
+    private float startTime;
+    private float distance;
+
     private void Awake() {
-        //panCam = GetComponentInChildren<Camera>();
-        //panCam = transform.GetChild(0);
         box = GetComponent<BoxCollider2D>();
         activated = false;
         listIndex = 0;
@@ -25,59 +28,64 @@ public class CameraPan : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
+            // disable player movement and log main camera's position
             player.GetComponent<IsometricPlayerMovement>().toggleMovement(false);
             mainCamPos = mainCam.transform.position;
 
+            // activate panning camera and deactivate main camera
             panCam.gameObject.SetActive(true);
             mainCam.gameObject.SetActive(false);
 
+            // bring panning camera to main camera and insert it's position into the positions list
             panCam.position = mainCamPos;
             panPositions.Insert(0, panCam.position);
+
+            // start timer and activate panning
+            startTime = Time.time;
+            goingBack = false;
             activated = true;
         }
     }
 
-    private void getNext() {
-        print(listIndex);
-        if(listIndex < panPositions.Count - 1)
+    private void getNextPosition() {
+        // get distance to next point and update index depending on if moving forward or backwards
+        if(listIndex < panPositions.Count - 1 && !goingBack) {
+            distance = Vector3.Distance(panCam.position, panPositions[listIndex + 1]);
             listIndex++;
-        else
+        }
+        else {
+            distance = Vector3.Distance(panCam.position, panPositions[listIndex - 1]);
             listIndex--;
+            goingBack = true;
+        }
+        startTime = Time.time;
     }
 
     private void Update() {
         if(activated) {
-            //Vector3 newPos = getNext();
-            //panCam.transform.position = Vector3.Lerp(panCam.transform.position, newPos, speed * Time.deltaTime);
-            panCam.position = Vector3.Lerp(panCam.position, panPositions[listIndex], speed * Time.deltaTime);
-            if(listIndex >= 0 && getDistance(panCam.position, panPositions[listIndex]) < 0.1) {//panCam.position == panPositions[listIndex]) {
-                print("yo");
-                getNext();
-            }
-            if(getDistance(panCam.position, panPositions[0]) < 0.1 && listIndex == 0) {
+            // get next position if current position reached
+            if(panCam.position == panPositions[listIndex])
+                getNextPosition();
+            
+            // lerp camera based on speed and distance covered
+            float distanceCovered = (Time.time - startTime) * speed;
+            float distanceFraction = distanceCovered / distance;
+            panCam.position = Vector3.Lerp(panCam.position, panPositions[listIndex], distanceFraction);
+
+            // if camera reaches original position
+            if(panCam.position == panPositions[listIndex] && listIndex == 0) {
+                // toggle player movement
                 player.GetComponent<IsometricPlayerMovement>().toggleMovement(true);
-                panCam.gameObject.SetActive(false);
+
+                // reset back to main camera
                 mainCam.gameObject.SetActive(true);
+                panCam.gameObject.SetActive(false);
+
+                // reset list, and deactivate game object
+                panPositions.RemoveAt(0);
                 activated = false;
                 gameObject.SetActive(false);
             }
         }
     }
-
-    private float getDistance(Vector3 vec1, Vector3 vec2) {
-        float x = Mathf.Pow(vec2.x - vec1.x, 2);
-        float y = Mathf.Pow(vec2.y - vec1.y, 2);
-        float z = Mathf.Pow(vec2.z - vec1.z, 2);
-        return Mathf.Sqrt(x + y + z);
-    }
-
-    // disable player movement
-    // enable pan cam
-    // disable main cam
-    // lerp pan cam to each vector in list
-    // lerp pan cam through list backwards
-    // enable main cam
-    // disable pan cam
-    // enable player movement
-    // disable game object
 }
